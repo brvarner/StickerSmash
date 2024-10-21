e51122d8479a557f3c45e77efaa6a82eaa737b32
Brandon Varner <84243251+brvarner@users.noreply.github.com>
2024-10-20 20:30:18 -0500

    added image viewer component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
-import { Image } from "expo-image";
+import ImageViewer from "@/components/ImageViewer";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -8,7 +7,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
       </View>
     </View>
   );
@@ -22,13 +21,5 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
-  },
-  text: {
-    color: "#fff",
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
   },
 });
